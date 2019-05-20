8bcc3135a8a95ed6c9e2312e6f0777406bc54eed
Nico Koenig <nico@koenig.codes>
2019-05-20 10:26:08 +0200

    test: adds test cases

--- javascript/katas/caesar/tests.js
+++ javascript/katas/caesar/tests.js
@@ -2,8 +2,11 @@ const { describe, it, equals } = require("./../../test-framework/framework");
 const { caesarChipher } = require("./caesar");
 
 describe("caesar chipher", () => {
-  const testData = [{ input: "ab", shift: 1, output: "bc" }];
-  const testData = [{ input: "KrautIpsum", shift: -1, output: "JqztHortl" }];
+  const testData = [
+    { input: "ab", shift: 1, output: "bc" },
+    { input: "KrautIpsum", shift: -1, output: "JqztsHortl" },
+    { input: "A", shift: -1, output: "Z" }
+  ];
 
   testData.forEach(testDataItem => {
     it(`should test the caesarChipher function with value "input:${
